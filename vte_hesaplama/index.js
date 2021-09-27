eb3bdfd731c3ad488b84109bd83a4e329bea21c8
Newt0n <47007924+Blackcloud00@users.noreply.github.com>
2021-09-28 00:30:03 +0300

    Vücut Kitle Endeksi hesaplama programı
    Vücut Kitle Endeksi hesaplama programı html/css/javascript

---
+++ vte_hesaplama/index.js
@@ -0,0 +1,18 @@
+function hesapla(){
+    var weight  = document.getElementById("weight").value;
+var height  = document.getElementById("height").value;
+
+var sonuc = weight/Math.pow(height,2);
+
+document.getElementById("sonuc").innerText=sonuc.toFixed(2);    
+
+if(sonuc<18.5){
+    document.getElementById("kategori").innerText="Under Weight";
+}else if(sonuc<=18.5 && sonuc<=25){
+    document.getElementById("kategori").innerText="Normal";
+}else if(sonuc<25 && sonuc<=30){
+    document.getElementById("kategori").innerText="Over Weight";
+}else if(sonuc>30){
+    document.getElementById("kategori").innerText="Obese";
+}
+}
